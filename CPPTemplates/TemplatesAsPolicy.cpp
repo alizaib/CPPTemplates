f5c6ec445bfefe98572c847ec076ddab1da26ec7
alizaib <alizaib@gmail.com>
2022-09-03 19:27:43 +0500

    Add project files.

---
+++ CPPTemplates/TemplatesAsPolicy.cpp
@@ -0,0 +1,36 @@
+#include "TemplatesAsPolicy.h"
+#include <string>
+
+class CaseSenCmp {
+public:
+	static int isEqual(char x, char y) {
+		return x == y;
+	}
+};
+
+class NonCaseSenCmp {
+public:
+	static int isEqual(const char x, const char y) {
+		return toupper(x) == toupper(y);
+	}
+};
+
+template<typename C>
+int compare(const char* s1, const char* s2) {
+	for(int i=0; i<strlen(s1) && i<strlen(s2); i++) {		
+		if (!C::isEqual(s1[i], s2[i])) { // No intellisense will be provided here, as in C#
+			return s1[i] - s2[i];
+		}
+	}
+
+	return strlen(s1) - strlen(s2);
+}
+
+TemplatesAsPolicy::TemplatesAsPolicy() {
+	int i, j;
+	const char* x = "hello", * y = "HELLO";
+	i = compare<CaseSenCmp>(x, y);
+	j = compare<NonCaseSenCmp>(x, y);
+	cout << "Case Sensitive: " << i;
+	cout << "\nNon-Case Sensitive: " << j << endl;
+}
---
+++ CPPTemplates/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "TemplatesAsPolicy.h"
+
+using namespace std;
+
+int main() {
+	TemplatesAsPolicy t1;
+}
+
